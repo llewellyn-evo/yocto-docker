f973ef053d1dc4b902544ffdc6a4f5296ebb56f1
Victor Voronin <viktor.voronin@evologics.de>
2020-02-05 12:27:57 +0100

    machine/tx6: add navimum-swom files

---
+++ machine/tx6-evobb-common/navimum-swom.mk
@@ -0,0 +1,6 @@
+include machine/tx6-evobb-common/qemu-target.mk
+
+LAYERS += $(SOURCES_DIR)/meta-evo-private
+LOCAL_CONF_OPT += 'MACHINEOVERRIDES =. "navimum-swom:navimum-swom-$${EVOSN}:"' \
+                  'CORE_IMAGE_EXTRA_INSTALL += "dune-navimum-swom-$${EVOSN} dune-etc-navimum-swom-$${EVOSN} dune-etc-common dune-etc-wmm"' \
+				  'hostname_pn-base-files = "navimum-swom-$${EVOSN}"'
