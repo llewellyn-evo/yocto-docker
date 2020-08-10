bbb8509f72d2c48709147542ad22b53285c44f96
Victor Voronin <viktor.voronin@evologics.de>
2020-08-10 17:15:56 +0200

    machine: add sonobot-18-payload on colibri-imx6-iris-evo

---
+++ machine/colibri-imx6-iris-evo/sonobot-18-payload.mk
@@ -0,0 +1,13 @@
+# Board defaults
+include machine/colibri-imx6-iris-evo/default.mk
+
+# Vehicle common defs
+include machine/append-hw-common/sonobot-payload.mk
+
+# Software common defs
+#include machine/append-sw-common/qemu-target.mk
+include machine/append-sw-common/hyscan.mk
+include machine/append-sw-common/dune.mk
+
+# Vehicle number
+LOCAL_CONF_OPT += 'EVOSN = "18"'
