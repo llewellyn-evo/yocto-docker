65a48ef0a5f641d4255a1bb1aab3f608c24ce1ae
Llewellyn Fernandes <llewellynkbc@gmail.com>
2020-08-29 23:34:52 +0200

    Added configs for poggy and sonobot v5

---
+++ machine/append-hw-common/poggy-comm.mk
@@ -0,0 +1,5 @@
+include machine/append-sw-common/qemu-target.mk
+
+LOCAL_CONF_OPT += 'hostname_pn-base-files = "poggy-comm-$${EVOSN}"'
+
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS = "can"'
---
+++ machine/append-hw-common/sonobot-comm.mk
@@ -0,0 +1,5 @@
+include machine/append-sw-common/qemu-target.mk
+
+LOCAL_CONF_OPT += 'hostname_pn-base-files = "sonobot-v5-comm-$${EVOSN}"'
+
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS = "can"'
