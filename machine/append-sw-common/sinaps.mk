a03a44a2c8fc3a19a18f3a2ea3442d2a39cca605
Victor Voronin <viktor.voronin@evologics.de>
2020-11-06 14:22:04 +0100

    machine/append-sw-common: add sinaps.mk

---
+++ machine/append-sw-common/sinaps.mk
@@ -0,0 +1,10 @@
+# Erlang comes from here
+LAYERS += $(SOURCES_DIR)/meta-erlang
+LOCAL_CONF_OPT += 'PREFERRED_VERSION_erlang = "21.1%"'
+LOCAL_CONF_OPT += 'PREFERRED_VERSION_erlang-native = "21.1%"'
+
+# SiNAPS lives in meta-evo-private
+LAYERS += $(SOURCES_DIR)/meta-evo-private
+
+# Install necessary packages.
+LOCAL_CONF_OPT += 'CORE_IMAGE_EXTRA_INSTALL += "sinaps"'
