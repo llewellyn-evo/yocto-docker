7bc0b9feda540fdddb7ac6660c7b54690f13e04b
Victor Voronin <viktor.voronin@evologics.de>
2020-11-11 12:49:19 +0100

    machine/tx6q-8137-evobb: add sonobot-5001.mk

---
+++ machine/tx6q-8137-evobb/sonobot-5001.mk
@@ -0,0 +1,13 @@
+# Board defaults
+include machine/tx6q-8137-evobb/default.mk
+
+# Vehicle common defs
+include machine/append-hw-common/sonobot.mk
+
+# Software common defs
+include machine/append-sw-common/qemu-target.mk
+include machine/append-sw-common/hyscan.mk
+include machine/append-sw-common/dune.mk
+
+# Vehicle number
+LOCAL_CONF_OPT += 'EVOSN = "5001"'
