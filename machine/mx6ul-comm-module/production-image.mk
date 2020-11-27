335eb88b5dc6ba9f3fcfc4c6051599db50521130
llewellyn-evo <llewellyn.fernandes@evologics.de>
2020-11-27 21:44:44 +0100

    Added default production image configuration

---
+++ machine/mx6ul-comm-module/production-image.mk
@@ -0,0 +1,6 @@
+include machine/mx6ul-comm-module/default.mk
+include machine/append-sw-common/update.mk
+
+LOCAL_CONF_OPT += 'hostname_pn-base-files = "comm-module-base-image"'
+
+LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
