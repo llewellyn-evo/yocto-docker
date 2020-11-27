cfe44e7d50a5c89cd8f75600fcc5657681aa3d53
llewellyn-evo <llewellyn.fernandes@evologics.de>
2020-11-27 21:43:32 +0100

    added sw config for update mechanism

---
+++ machine/append-sw-common/update.mk
@@ -0,0 +1,7 @@
+include machine/append-sw-common/qemu-target.mk
+
+LAYERS += $(SOURCES_DIR)/meta-evo-private
+
+#Install SWUPDATE
+LOCAL_CONF_OPT    += 'IMAGE_INSTALL_append = " swupdate swupdate-www"'
+LOCAL_CONF_OPT 	  += 'PREFERRED_VERSION_swupdate = "git"'
