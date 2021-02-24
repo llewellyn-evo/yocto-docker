43511fec528ed365c029ec3300c30c1e1207ca50
Victor Voronin <viktor.voronin@evologics.de>
2021-02-24 16:24:08 +0100

    machine/tx6q-8137-evobb: add evotools to sonobot-500{1,2,3}.mk

--- machine/tx6q-8137-evobb/sonobot-5002.mk
+++ machine/tx6q-8137-evobb/sonobot-5002.mk
@@ -8,6 +8,7 @@ include machine/append-hw-common/sonobot-r5.mk
 include machine/append-sw-common/qemu-target.mk
 include machine/append-sw-common/hyscan.mk
 include machine/append-sw-common/dune.mk
+include machine/append-sw-common/evotools.mk
 
 # Vehicle number
 LOCAL_CONF_OPT += 'EVOSN = "5002"'
