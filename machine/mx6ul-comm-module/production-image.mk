dda08eaf80030c938d69d94d4f8b5b8918a8ec6d
LLewellyn Fernandes <llewellyn.fernandes@evologics.de>
2021-07-18 13:48:43 +0200

    Enable can and power switches in production image

--- machine/mx6ul-comm-module/production-image.mk
+++ machine/mx6ul-comm-module/production-image.mk
@@ -3,4 +3,6 @@ include machine/append-sw-common/update.mk
 
 LOCAL_CONF_OPT += 'hostname_pn-base-files = "comm-module-base-image"'
 
+LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " can enablesw"'
+
 LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
