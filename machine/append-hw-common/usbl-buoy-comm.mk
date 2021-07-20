dcc5113e0267f5ed4728be869b34c450a803249e
llewellyn-evo <llewellyn.fernandes@evologics.de>
2021-07-20 19:12:32 +0200

    added configs for sonobot 31-32 and basestations 31-32

--- machine/append-hw-common/usbl-buoy-comm.mk
+++ machine/append-hw-common/usbl-buoy-comm.mk
@@ -1,10 +1,9 @@
 include machine/append-sw-common/qemu-target.mk
+include machine/append-sw-common/sinaps.mk
 
 LAYERS += $(SOURCES_DIR)/meta-evo-private
 
 LOCAL_CONF_OPT += 'INHERIT += " userconfig"'
-
-LOCAL_CONF_OPT += 'IMAGE_ROOTFS_EXTRA_SPACE = "50000"'
 
 LOCAL_CONF_OPT += 'BRIDGE_ADDRESS="192.168.0.2"'
 
---
+++ machine/mx6ul-comm-module/sonobot-5031.mk
@@ -0,0 +1,4 @@
+include machine/mx6ul-comm-module/default.mk
+include machine/append-hw-common/sonobot-comm.mk
+
+LOCAL_CONF_OPT += 'EVOSN = "5031"'
