7babd7815681d64d4b51c84c4dbe02d23853deb6
llewellyn-evo <llewellyn.fernandes@evologics.de>
2021-08-10 18:22:47 +0200

    Add Dune to commmodule production image

--- machine/mx6ul-comm-module/production-image.mk
+++ machine/mx6ul-comm-module/production-image.mk
@@ -1,8 +1,10 @@
 include machine/mx6ul-comm-module/default.mk
 include machine/append-sw-common/update.mk
 
-LOCAL_CONF_OPT += 'hostname_pn-base-files = "comm-module-base-image"'
+LOCAL_CONF_OPT += 'hostname_pn-base-files = "production-comm-image"'
 
 LOCAL_CONF_OPT += 'IMAGE_CONFIGS = " can enablesw"'
+
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-production-comm dune-production-comm-etc dune-production-comm-default"'
 
 LOCAL_CONF_OPT += 'HW_REVISION = "2.0"'
