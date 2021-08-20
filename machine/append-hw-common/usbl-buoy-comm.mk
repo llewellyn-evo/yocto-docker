23524a084e0634a28dc7d93f72b0cc3b5167a3a5
llewellyn-evo <llewellyn.fernandes@evologics.de>
2021-08-20 13:09:29 +0200

    Add dune to usbl-buoy

--- machine/append-hw-common/usbl-buoy-comm.mk
+++ machine/append-hw-common/usbl-buoy-comm.mk
@@ -1,13 +1,11 @@
-include machine/append-sw-common/qemu-target.mk
+include machine/append-sw-common/sinaps.mk
 
 LAYERS += $(SOURCES_DIR)/meta-evo-private
 
 LOCAL_CONF_OPT += 'INHERIT += " userconfig"'
 
-LOCAL_CONF_OPT += 'IMAGE_ROOTFS_EXTRA_SPACE = "50000"'
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " htop soft-hwclock"'
 
-LOCAL_CONF_OPT += 'BRIDGE_ADDRESS="192.168.0.2"'
-
-LOCAL_CONF_OPT += 'BRIDGE_GATEWAY="192.168.0.1"'
+LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-usbl-buoy-$${EVOSN} dune-usbl-buoy-$${EVOSN}-etc dune-usbl-buoy-$${EVOSN}-default"'
 
 LOCAL_CONF_OPT += 'hostname_pn-base-files = "usbl-buoy-comm-$${EVOSN}"'
