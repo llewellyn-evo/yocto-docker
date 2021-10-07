9a00b174431dd7f78f39f834c6884838169859d6
llewellyn-evo <llewellyn.fernandes@evologics.de>
2021-10-07 18:23:52 +0200

    Enable Switches and can in fast penguin images

--- machine/append-hw-common/fast-penguin.mk
+++ machine/append-hw-common/fast-penguin.mk
@@ -5,5 +5,3 @@ LAYERS += $(SOURCES_DIR)/meta-evo-private
 LOCAL_CONF_OPT += 'IMAGE_INSTALL_append = " dune-fast-penguin-$${EVOSN} dune-fast-penguin-$${EVOSN}-etc dune-fast-penguin-$${EVOSN}-default"'
 
 LOCAL_CONF_OPT += 'hostname_pn-base-files = "fast-penguin-$${EVOSN}"'
-
-LOCAL_CONF_OPT += 'IMAGE_CONFIGS = "can"'
