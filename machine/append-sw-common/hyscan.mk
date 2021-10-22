e5f2851f48fa60c9f4ede1d032245f9e1869003f
Maksym Komar <komar@evologics.de>
2021-10-22 12:31:57 +0200

    Update links to meta-evo-private and meta-hyscan

--- machine/append-sw-common/hyscan.mk
+++ machine/append-sw-common/hyscan.mk
@@ -1,5 +1,5 @@
 # HyScan layer.
-LAYERS += $(SOURCES_DIR)/meta-hyscan
+LAYERS += git@gitlab.evologics.de:bsp/meta-hyscan.git
 
 # Install necessary packages.
 LOCAL_CONF_OPT += 'CORE_IMAGE_EXTRA_INSTALL += "hyscansonarproxyd hyscanhydra4drv hyscannmeadrv pure-ftpd"'
